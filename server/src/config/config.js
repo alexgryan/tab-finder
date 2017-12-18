9b8ba2fab417dd3370c01859a3161a4aa4b51a86
Alex Ryan <alexryan1995@gmail.com>
2017-12-17 23:49:21 -0700

    Added hashing/encryption to Login and Register components of Vue.js app

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -9,5 +9,8 @@ module.exports = {
             host: process.env.HOST || 'localhost',
             storage: './tabfinder.sqlite'
         }
+    },
+    authentication: {
+        jwtSecret: process.env.JWT_SECRET || 'secret'
     }   
 }
