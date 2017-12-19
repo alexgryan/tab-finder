7b55f0dddc9e3e0160421d93b0c149cef1cce0bf
Alex Ryan <alexryan1995@gmail.com>
2017-12-18 21:50:23 -0700

    defined Song database schema and created Api services for getting and adding Songs to database of Vue Js app

--- client/src/components/Panel.vue
+++ client/src/components/Panel.vue
@@ -1,26 +1,23 @@
 <template>
-  <v-flex xs6 offset-xs3>
-    <div class="white elevation-2">
-      <v-toolbar flat dense class="cyan" dark>
-        <v-toolbar-title>{{title}}</v-toolbar-title>
-          </v-toolbar>
+  <div class="white elevation-2">
+    <v-toolbar flat dense class="cyan" dark>
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+      <slot name="action" />
+    </v-toolbar>
 
-        <div class="pl-4 pr-4 pt-2 pb-2">
-          <slot>
-            No slot content right now.
-          </slot>
-        </div>
+    <div class="pl-4 pr-4 pt-2 pb-2">
+      <slot>
+        No slot content defined.
+      </slot>
     </div>
-  </v-flex>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      title: 'working'
-    }
-  }
+  props: [
+    'title'
+  ]
 }
 </script>
 
--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -1,16 +1,33 @@
 <template>
   <v-layout column>
     <v-flex xs6 offset-xs3>
-      <panel />
+      <panel title = "Songs">
+        <div v-for="song in songs"
+          :key = "song.title">
+          {{song.title}}
+          {{song.artist}}
+          {{song.album}}
+        </div>
+      </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import SongsService from '@/services/SongsService'
 import Panel from '@/components/Panel'
 export default {
   components: {
     Panel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  }
+
+  async mounted () {
+    this.songs = (await SongsService.index()).data
   }
 }
 </script>
