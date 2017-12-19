426cff5c3bc0ef87e240fc894dc33e91fbeff0fa
Alex Ryan <alexryan1995@gmail.com>
2017-12-19 00:25:25 -0700

    Created Add Song functionality with rules and created a listing mechanism for songs that are currently populated the databse in the Vue js app

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,8 +2,52 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <panel title = "Songs">
+        <v-btn 
+          slot="action"
+          @click="navigateTo({name: 'songs-create'})"
+          class="cyan accent-2"
+          light
+          medium
+          absolute
+          right
+          middle
+          fab>
+          <v-icon>add</v-icon>
+        </v-btn>
+
         <div v-for="song in songs"
+          class="song"
           :key = "song.title">
+
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-genre">
+                {{song.genre}}
+              </div>
+              <v-btn
+                dark
+                class="cyan"
+                @click="navigateTo({
+                  name: 'song', 
+                  params: {
+                    songId: song.id
+                    }
+                  })">
+                  View
+              </v-btn>
+            </v-flex>
+
+            <v-flex xs6>
+              <img class="album-image" :src="song.albumImageUrl" />  
+            </v-flex>
+          </v-layout>
+
           {{song.title}}
           {{song.artist}}
           {{song.album}}
@@ -24,8 +68,13 @@ export default {
     return {
       songs: null
     }
-  }
+  },
 
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  },
   async mounted () {
     this.songs = (await SongsService.index()).data
   }
@@ -33,4 +82,26 @@ export default {
 </script>
 
 <style scoped>
+.song {
+  padding: 20px;
+  height: 300px;
+  overflow: hidden;
+}
+
+.song-title {
+  font-size: 30px;
+}
+
+.song-artist {
+  font-size: 24px;
+}
+
+.song-genre {
+  font-size: 18px;
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
 </style>
--- client/src/services/SongsService.js
+++ client/src/services/SongsService.js
@@ -3,5 +3,8 @@ import Api from '@/services/Api'
 export default {
   index (credentials) {
     return Api().get('songs')
+  },
+  post (song) {
+    return Api().post('songs', song)
   }
 }
